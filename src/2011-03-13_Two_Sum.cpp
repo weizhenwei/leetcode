5be601ee988f57d184f4f6f4c6f2005789348bf8
weizhenwei <weizhenwei-hf@loongson.cn>
2014-10-21 22:13:44 +0800

    add TwoSum solution

---
+++ src/2011-03-13_Two_Sum.cpp
@@ -0,0 +1,123 @@
+/*
+ *
+ * Copyright (c) 2014, weizhenwei
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of the {organization} nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * File: 2011-03-13_Two_Sum.cpp
+ *
+ *
+ * Brief: https://oj.leetcode.com/problems/two-sum/
+ * Given an array of integers, find two numbers such that they add up to a
+ * specific target number. The function twoSum should return indices of the
+ * two numbers such that they add up to the target, where index1 must be
+ * less than index2. Please note that your returned answers
+ * (both index1 and index2) are not zero-based.
+ * 
+ * You may assume that each input would have exactly one solution.
+ * 
+ * Input: numbers={2, 7, 11, 15}, target=9
+ * Output: index1=1, index2=2
+ *
+ *
+ * Date: 2014.05.10
+ *
+ * Author: weizhenwei <weizhenwei1988@gmail.com>
+ *
+ * *****************************************************************************
+ */
+
+#include <stdio.h>
+#include <vector>
+#include <map>
+
+using std::vector;
+using std::map;
+
+class Solution_Two_Sum {
+public:
+    vector<int> twoSum_stupid(vector<int> &numbers, int target) {
+        int i = 0;
+        vector<int> result;
+        for (i = 0; i < numbers.size(); i++) {
+            int a = target - numbers[i];
+            for (int j = i + 1; j < numbers.size(); j++) {
+                if (numbers[j] == a) {
+                    result.push_back(i + 1);
+                    result.push_back(j + 1);
+                    break;
+                } // if
+            } // for
+        }  // for 
+
+        return result;
+    }
+
+    vector<int> twoSum_clever(vector<int> &numbers, int target) {
+        int i = 0;
+        vector<int> result;
+        map<int, int> tmp;
+
+        // first make value and position hashing;
+        for ( i = 0; i < numbers.size(); i++) {
+            tmp[numbers[i]] = i;
+        }
+
+        // then check;
+        for (i = 0; i < numbers.size(); i++) {
+            int a = target - numbers[i];
+            if ((tmp.find(a) != tmp.end()) && (tmp[a] > i)) {
+                result.push_back(i + 1);
+                result.push_back(tmp[a] + 1);
+                break;
+            } // if
+        }  // for 
+
+        return result;
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Solution_Two_Sum twosum;
+    vector<int> numbers;
+    numbers.push_back(2);
+    numbers.push_back(7);
+    numbers.push_back(11);
+    numbers.push_back(15);
+
+    vector<int> result1 = twosum.twoSum_stupid(numbers, 13);
+    for (int i = 0; i < result1.size(); i++) {
+        printf("%d\n", result1[i]);
+    }
+
+    vector<int> result2 = twosum.twoSum_clever(numbers, 13);
+    for (int i = 0; i < result2.size(); i++) {
+        printf("%d\n", result2[i]);
+    }
+
+    return 0;
+}
