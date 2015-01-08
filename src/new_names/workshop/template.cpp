b5bc4b93a3a2c2f372d7b947316f48f6e420cf66
weizhenwei <weizhenwei-hf@loongson.cn>
2015-01-08 21:38:26 +0800

    add today's work to Makefile

--- src/new_names/workshop/template.cpp
+++ src/new_names/workshop/template.cpp
@@ -28,10 +28,10 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * File: 2011-03-13_Two_Sum.cpp
+ * File: 173-Binary_Search_Tree_Iterator.cpp
  *
  *
- * Brief: https://oj.leetcode.com/problems/two-sum/
+ * Brief: https://oj.leetcode.com/problems/binary-search-tree-iterator/
  * Given an array of integers, find two numbers such that they add up to a
  * specific target number. The function twoSum should return indices of the
  * two numbers such that they add up to the target, where index1 must be
